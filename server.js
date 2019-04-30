01566514abd84229b227424f2bd2e7a68200931b
Floyd Orr <floydcanhelp@gmail.com>
2019-04-30 10:37:55 -0500

    pages wired

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var bodyParser = require('body-parser')
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+app.get('/', (req, res) => {
+  res.render('./pages/index');
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
